79c680a1ec7679c45fe657f4f574424f1996d7ef
Vineeth <vineethkumarsahoo08@gmail.com>
2025-08-27 16:14:17 +0530

    Fix project structure and workflow

--- AutoFillApp/app/src/main/java/com/example/autofillapp/MainActivity.kt
+++ AutoFillApp/app/src/main/java/com/example/autofillapp/MainActivity.kt
@@ -1,11 +1,12 @@
-package com.example.autofillapp
+package com.example.autofillapp;
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
 }
